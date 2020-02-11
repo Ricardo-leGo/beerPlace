42941cddf155b8b43184675f6902488a8b52888d
Ricardo-leGo <sofiamejiamuro@gmail.com>
2020-02-11 15:56:05 -0600

    Agregamos Img a ModelSchema User

--- models/User.js
+++ models/User.js
@@ -1,11 +1,15 @@
 const {model, Schema} = require('mongoose')
 const PLM             = require('passport-local-mongoose')
 
-
+let varImg = Math.floor(Math.random()*450)
 const userSchema = new Schema(
   {
     email:    String,
     name:     String,
+    img:{
+      type:String,
+      default:`https://i.picsum.photos/id/${varImg}/400/400.jpg`
+    },
     token:    String,
     googleID: String,
     image: {
